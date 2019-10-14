757cbb8d31de305cc16b5c3c2c6598e76d7eed69
Emilio Kartono <emiliokartono@gmail.com>
2019-10-14 15:15:37 -0400

    got rid of country filter

--- ML Model/src/main.py
+++ ML Model/src/main.py
@@ -88,11 +88,12 @@ def get_dataset():
     countries = [ country_id_to_country[id][0] for id in country_id_to_country ]
 	
     records = [( record[0], country_id_to_country[record[1]][0] ) for record in get_records()]
-    records = list(filter(lambda x: x[1] == 'China' or x[1] == 'United Kingdom', records))
-    countries = ["China", "United Kingdom"]
+    # records = list(filter(lambda x: x[1] == 'China' or x[1] == 'United Kingdom', records))
+    # countries = ["China", "United Kingdom"]
+    
     np.random.shuffle(records)
 
-    records = records[0:8000]
+    # records = records[0:8000]
 	
     # Splits the records into two lists
     examples = [ record[0] for record in records ]
