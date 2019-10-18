d0dcab9e4e2be18bb3c257c24f7ae07773c41cb9
Emilio Kartono <emiliokartono@gmail.com>
2019-10-18 00:33:07 -0400

    wip

--- ML Model/src/main.py
+++ ML Model/src/main.py
@@ -101,7 +101,7 @@ def get_dataset():
     records = [( record[0], country_id_to_country[record[1]][0] ) for record in records]
     
     np.random.shuffle(records)
-    records = records[0:4000]
+    records = records[0:8000]
 	
     # Splits the records into two lists
     examples = [ record[0] for record in records ]
@@ -121,8 +121,9 @@ def main():
 
     for hidden_layers_count in various_hidden_layers_count:
         classifier = NamesToNationalityClassifier(countries, 
+                                                  alpha=0.001,
                                                   hidden_dimensions=hidden_layers_count, 
-                                                  momentum=0.5, # Before was 0.1 
+                                                  momentum=0.9, # Before was 0.1 
                                                   num_epoche=100, 
                                                   l2_lambda=0.02)
 
