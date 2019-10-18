34159aa332df0436a8506a76c92577f2b82796f9
Emilio Kartono <emiliokartono@gmail.com>
2019-10-18 09:52:06 -0400

    wip

--- ML Model/Train-ML.sh
+++ ML Model/Train-ML.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python3 -u src/main.py > logs.txt
-mail -a *-plots.png -s "Training Model Done" e_kartono@hotmail.ca
+mail -A *-plots.png -s "Training Model Done" e_kartono@hotmail.ca
