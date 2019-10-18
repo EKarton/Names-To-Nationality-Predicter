6dcb3a8184b61804bf8257a60a8a76e771bf5122
Emilio Kartono <emiliokartono@gmail.com>
2019-10-18 09:57:41 -0400

    wip

--- ML Model/src/main.py
+++ ML Model/src/main.py
@@ -101,7 +101,7 @@ def get_dataset():
     records = [( record[0], country_id_to_country[record[1]][0] ) for record in records]
     
     np.random.shuffle(records)
-    records = records[0:10]
+    records = records[0:4000]
 	
     # Splits the records into two lists
     examples = [ record[0] for record in records ]
