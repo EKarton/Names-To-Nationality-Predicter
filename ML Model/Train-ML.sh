e8c1d1f921c7216a5c06a701572a3236d9a04666
Emilio Kartono <emiliokartono@gmail.com>
2019-10-18 21:43:25 -0400

    wip

--- ML Model/Train-ML.sh
+++ ML Model/Train-ML.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+ps aux | grep kartonoe
+
+kill -9 $("ps aux | grep kartonoe | grep python | cut -d ' ' -f 2")
+git add --all
+git stash
+git pull
+rm -rf logs.txt
+rm -rf *-plots.png
+
 python3 -u src/main.py > logs.txt
 mail -s "Training Model Done" e_kartono@hotmail.ca
