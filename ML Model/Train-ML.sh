6f17b63053ddeee334fbb9b42bf60894034725a9
Emilio Kartono <emiliokartono@gmail.com>
2019-10-18 21:44:31 -0400

    wip

--- ML Model/Train-ML.sh
+++ ML Model/Train-ML.sh
@@ -2,7 +2,14 @@
 
 ps aux | grep kartonoe
 
+sleep 10
+
 kill -9 $("ps aux | grep kartonoe | grep python | cut -d ' ' -f 2")
+
+sleep 10
+
+ps aux | grep kartonoe
+
 rm -rf logs.txt
 rm -rf *-plots.png
 
